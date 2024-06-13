a84feb54df7445724fbb003f18f7c7ffe8e5c4b1
Kacpers27325 <s27325@pjwstk.edu.pl>
2024-06-13 18:17:49 +0200

    AA

---
+++ WebApplication1/WebApplication1/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Database;
+using WebApplication1.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add controllers functionality
+builder.Services.AddControllers();
+// Dependency injection for AnimalsRepository
+
+builder.Services.AddDbContext<DatabaseContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddScoped<IDbService, DbService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Map controllers endpoints
+app.MapControllers();
+
+app.Run();
